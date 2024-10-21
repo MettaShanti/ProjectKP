2d7694157c2a9aaad99f9e21cd47b19425482655
MettaShanti <mettashanti_2225110011@mhs.mdp.ac.id>
2024-10-21 18:00:31 +0700

    db

---
+++ Absensi_PtPinagoUtama/Absensi/app/Http/Controllers/Pembagian3Controller.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pembagian3;
+use Illuminate\Http\Request;
+
+class Pembagian3Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pembagian3 $pembagian3)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pembagian3 $pembagian3)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pembagian3 $pembagian3)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pembagian3 $pembagian3)
+    {
+        //
+    }
+}
